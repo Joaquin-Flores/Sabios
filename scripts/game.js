8394c95c6bc4a3f8209e5a268e9dbe537d437d48
Joaquin-Flores <108761029+Joaquin-Flores@users.noreply.github.com>
2025-03-16 01:45:46 -0300

    más lógica de juego
    Ahora se puede ganar, elegir opciones correctas y llevar un registro de que preguntas voy respondiendo bien. Además de implementar que no se repitan las preguntas ya mencionadas. Lógica de puntos, vidas, temporizador, mensaje, etc.

--- scripts/game.js
+++ scripts/game.js
@@ -1,17 +1,25 @@
 let preguntas = [];
+let pregunta;
+let temporizador;
 let puntaje = 0;
 let racha = 0;
 let vidas = 3;
-let tiempo = 60;
-let indicePregunta = -1;
-let dificultad = "Medio";
-let categoriaActual = "Historia de la Iglesia";
-let preguntasRespondidas = 0;
+let tiempo = 20;
+let dificultad = "Fácil";
+const categorias = ["Historia de la Iglesia", "Doctrina", "Santos", "Biblia"];
+let categoriaActual = categorias[3];
+let progreso = {
+    "Historia de la Iglesia": 0,
+    "Doctrina": 0,
+    "Santos": 0,
+    "Biblia": 0,
+    "FinalBoss": false
+};
 
 async function cargarPreguntas() {
     try {
-        const respuesta = await fetch("/scripts/preguntas.json");
-        preguntas = await respuesta.json();
+        const response = await fetch("/scripts/preguntas.json");
+        preguntas = await response.json();
         iniciarJuego();
     } catch (error) {
         console.error('Error al cargar el archivo JSON:', error);
@@ -21,12 +29,18 @@ async function cargarPreguntas() {
 function iniciarJuego() {
     proximaPregunta();
     iniciarTiempo();
+    reiniciarColores();
+}
+
+function reiniciarColores() {
+    for (let index = 0; index < 4; index++) {
+        document.getElementById("respuesta" + index).style.backgroundColor = "#ffd900";
+    }
 }
 
 function proximaPregunta() {
     categoriaActual = categoriaRandom();
-    let pregunta = preguntaRandom(categoriaActual, dificultad);
-    let respuesta = pregunta["correcta"];
+    pregunta = preguntaRandom(categoriaActual, dificultad);
     document.getElementById("categoria").innerText = categoriaActual;
     document.getElementById("pregunta").innerText = pregunta["pregunta"];
     document.getElementById("respuesta0").innerText = pregunta["respuestas"][0];
@@ -36,38 +50,100 @@ function proximaPregunta() {
 }
 
 function preguntaRandom(categoriaActual, dificultad) {
-    const listaPreguntas = preguntas[categoriaActual][dificultad];
-    return listaPreguntas[Math.floor(Math.random() * listaPreguntas.length)];
+    let listaPreguntas = preguntas[categoriaActual][dificultad];
+    if (listaPreguntas.length === 0) {
+        document.getElementById("pregunta").innerText = "Haz alcanzado la Sabiduría (no hay más preguntas)";
+        finDelJuego();
+    }
+    return listaPreguntas.splice(Math.floor(Math.random() * listaPreguntas.length), 1)[0];
 }
 
 function categoriaRandom() {
-    let sorteo = Math.floor(Math.random() * 5 );
-    switch (sorteo) {
-        case 1:
-            return "Historia de la Iglesia";
-            break;
-        case 2:
-            return "Doctrina";
-            break;
-        case 3:
-            return "Santos";
-            break;
-        case 4:
-            return "Biblia";
-            break;
-        default:
-            return "Historia de la Iglesia";
-            break;
-    }
+    return categorias[Math.floor(Math.random() * categorias.length)];
 }
 
 function iniciarTiempo() {
-    setInterval(() => {
+    if (temporizador) clearInterval(temporizador);
+    temporizador = setInterval(() => {
         if (tiempo > 0) {
             tiempo--;
             document.getElementById("tiempo").innerText = `⏳ ${tiempo}s`;
+        } else {
+            document.getElementById("pregunta").innerText = "TE QUEDASTE SIN TIEMPO!";
+            finDelJuego();
         }
     }, 1000);
+}
+
+function verificarRespuesta(respuestaSeleccionada) {
+    let respuesta = pregunta["correcta"];
+    if (respuestaSeleccionada === respuesta) {
+        racha++;
+        let multiplicador = racha >= 3 ? 2 : 1;
+        puntaje += 100 * multiplicador;
+        document.getElementById("respuesta" + respuesta).style.backgroundColor = "greenyellow";
+        document.getElementById("pregunta").innerText = "CORRECTO!";
+        if (categoriaActual === "Final Boss") {
+            progreso["FinalBoss"] = true;
+        } else {
+            progreso[categoriaActual]++;
+        }
+    } else {
+        vidas--;
+        puntaje -= 100;
+        racha = 0;
+        document.getElementById("respuesta" + respuestaSeleccionada).style.backgroundColor = "red";
+        document.getElementById("respuesta" + respuesta).style.backgroundColor = "greenyellow";
+        document.getElementById("pregunta").innerText = "INCORRECTO!";
+    }
+
+    document.getElementById("puntaje").innerText = `Puntaje: ${puntaje}`;
+    document.getElementById("vidas").innerText = "❤️".repeat(vidas) + "❌".repeat(3-vidas);
+
+    if (vidas <= 0) {
+        document.getElementById("pregunta").innerText = "TE QUEDASTE SIN VIDAS!";
+        finDelJuego();
+    } else {
+        setTimeout(() => {
+            verificarProgreso();
+        }, 2000);
+    }
+}
+
+function verificarProgreso() {
+    if (progreso["Historia de la Iglesia"] === 3 && progreso["Doctrina"] === 3 && progreso["Santos"] === 3 && progreso["Biblia"] === 3){
+        if (progreso["FinalBoss"] === true){
+            document.getElementById("categoria").innerText = "¡Ganaste!";
+            finDelJuego();
+        }
+        else {
+            iniciarFinalBoss();
+        }
+    }
+    else {
+        iniciarJuego();
+    }
+}
+
+function iniciarFinalBoss() {
+    dificultad = "Final Boss";
+    categoriaActual = categoriaRandom();
+    pregunta = preguntaRandom(categoriaActual, dificultad);
+
+    document.getElementById("categoria").innerText = `🔥 Final Boss: ${categoriaActual} 🔥`;
+    document.getElementById("pregunta").innerText = pregunta["pregunta"];
+    document.getElementById("respuesta0").innerText = pregunta["respuestas"][0];
+    document.getElementById("respuesta1").innerText = pregunta["respuestas"][1];
+    document.getElementById("respuesta2").innerText = pregunta["respuestas"][2];
+    document.getElementById("respuesta3").innerText = pregunta["respuestas"][3];
+}
+
+function finDelJuego() {
+    clearInterval(temporizador);
+    document.getElementById("pregunta").innerText = `¡Juego terminado! Tu puntaje final es: ${puntaje}`;
+    setTimeout(() => {
+        window.location.href = "/pages/index.html";
+    }, 3000);
 }
 
 document.addEventListener("DOMContentLoaded", cargarPreguntas);
