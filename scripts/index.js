70798f9968bd1a8877d2d46fd28c10b655d851e2
Joaquin-Flores <joaquinflores0109@gmail.com>
2025-03-22 11:30:20 -0300

    Cambios en la estructura de archivos y en la estética del sitio
    Ya no hay un archivo .html para cada sección del sitio (contacto, creditos, sobre), sino que se muestra el contenido dentro del mismo archivo index.html usando tarjetas.
    Con esa misma lógica (no salir de index.html), hice una primer tarjeta donde solo diga el título y un gran botón Jugar, para que la música pueda comenzar en el momento que hacen click.
    Luego cambié algunos detalles css, como cambiar algunos valores de px a vh (para que los textos y espacios se adapten a las distintas pantallas); cambié el margen de los botones, para que no desborden de las pantallas más chicas; cambié el tamaño del texto de la categoría, de las vidas, el puntaje y el tiempo, porque los jugadores que probaron Sabios no lo llegaban a ver.

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,35 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let bgMusic = new Audio("assets/sounds/cantoGregoriano.m4a");
-  window.bgMusic = bgMusic;
-  window.bgMusic.loop = true;
-  window.bgMusic.play();
-});
-
 function clickJugar() {
-    document.getElementById("dificultades").style.display = "flex"
-    document.getElementById("botonJugar").style.display = "none"
+    mostrarTarjetaMenu();
+    let bgMusic = new Audio("assets/sounds/cantoGregoriano.m4a");
+    window.bgMusic = bgMusic;
+    window.bgMusic.loop = true;
+    window.bgMusic.play();
 }
 
-  function seleccionarDificultad(dificultad) {
-    sessionStorage.setItem("dificultad", dificultad);
-    window.location.href = "game.html";
+function seleccionarDificultad(dificultad) {
+  sessionStorage.setItem("dificultad", dificultad);
+  window.location.href = "game.html";
+}
+
+function mostrarTarjetaMenu() {
+  document.getElementById('tarjetaInicio').style.display = "none";
+  document.getElementById('tarjetaContacto').style.display = "none";
+  document.getElementById('tarjetaCreditos').style.display = "none";
+  document.getElementById('tarjetaSobre').style.display = "none";
+  document.getElementById('tarjetaMenu').style.display = "block";
+}
+
+function mostrarContacto() {
+  document.getElementById('tarjetaContacto').style.display = "block";
+  document.getElementById('tarjetaMenu').style.display = "none";
+}
+
+function mostrarCreditos() {
+  document.getElementById('tarjetaCreditos').style.display = "block";
+  document.getElementById('tarjetaMenu').style.display = "none";
+}
+
+function mostrarSobre() {
+  document.getElementById('tarjetaSobre').style.display = "block";
+  document.getElementById('tarjetaMenu').style.display = "none";
 }
