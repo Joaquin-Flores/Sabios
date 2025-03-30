81c4d6ee39ac03bf26f7cbc854ed9c0a705363d1
Joaquin-Flores <joaquinflores0109@gmail.com>
2025-03-30 15:38:06 -0300

    Implementación visual de la tienda + cambios en nombres de variables
    Nueva carpeta /assets/images, donde se guardan los PNG de los items y el favicon.
    Mostrar puntaje del usuario en el menú principal. Botón de tienda en el menú principal, que abre el modal de la tienda.
    En la tienda un header estático contiene el botón de cerrar tienda. En el cuerpo de la tienda se muestran en forma de grilla (grid) los distintos items.
    Se muestra, de cada item, la imágen y el título. Al hacer click de despliega más información: La descripción, la rareza, el precio y el botón de compra.
    La tienda ya es responsive.
    
    Es un primer prototipo, puede cambiar radicalmente en el futuro.

--- scripts/game.js
+++ scripts/game.js
@@ -1,27 +1,27 @@
-let preguntas = [];
-let pregunta;
-let temporizador;
-let puntaje = 0;
-let racha = 0;
-let vidas = 3;
-let tiempo = 20;
-let dificultad = sessionStorage.getItem("dificultad") || "Fácil";
-const categorias = ["Historia de la Iglesia", "Doctrina", "Santos", "Biblia"];
-let categoriaActual = categorias[3];
-let progreso = {
+let questions = [];
+let question;
+let timer;
+let score = 0;
+let streak = 0;
+let lives = 3;
+let time = 20;
+let difficulty = sessionStorage.getItem("difficulty") || "Fácil";
+const categories = ["Historia de la Iglesia", "Doctrina", "Santos", "Biblia"];
+let currentCategory = categories[3];
+let progress = {
     "Historia de la Iglesia": 0,
     "Doctrina": 0,
     "Santos": 0,
     "Biblia": 0,
     "FinalBoss": false
-};
+};  
 
-function prepararJuego() {
-    cargarPreguntas();
-    agregarEventosUI();
+function prepareGame() {
+    loadQuestions();
+    addUIEvents();
 }
 
-function agregarEventosUI() {
+function addUIEvents() {
     let bgMusic = new Audio("assets/sounds/gameMusic.m4a");
     window.bgMusic = bgMusic;
     window.bgMusic.volume = 0;
@@ -36,157 +36,158 @@ function agregarEventosUI() {
     
 }
 
-async function cargarPreguntas() {
+async function loadQuestions() {
     try {
-        const response = await fetch("scripts/preguntas.json");
-        preguntas = await response.json();
-        iniciarJuego();
+        const response = await fetch("scripts/questions.json");
+        questions = await response.json();
+        startGame();
     } catch (error) {
         console.error('Error al cargar el archivo JSON:', error);
     }
 }
 
-function iniciarJuego() {
-    proximaPregunta();
-    iniciarTiempo();
-    reiniciarColores();
+function startGame() {
+    nextQuestion();
+    startTimer();
+    restartColors();
 }
 
-function reiniciarColores() {
+function restartColors() {
     for (let index = 0; index < 4; index++) {
-        document.getElementById("respuesta" + index).style.backgroundColor = "#ffd900";
+        document.getElementById("answer-" + index).style.backgroundColor = "#ffd900";
     }
 }
 
-function proximaPregunta() {
-    categoriaActual = categoriaRandom();
-    pregunta = preguntaRandom(categoriaActual, dificultad);
-    document.getElementById("categoria").innerText = categoriaActual;
-    document.getElementById("pregunta").innerText = pregunta["pregunta"];
-    document.getElementById("respuesta0").innerText = pregunta["respuestas"][0];
-    document.getElementById("respuesta1").innerText = pregunta["respuestas"][1];
-    document.getElementById("respuesta2").innerText = pregunta["respuestas"][2];
-    document.getElementById("respuesta3").innerText = pregunta["respuestas"][3];
+function nextQuestion() {
+    currentCategory = randomCategory();
+    question = randomQuestion(currentCategory, difficulty);
+    document.getElementById("category").innerText = currentCategory;
+    document.getElementById("question").innerText = question["pregunta"];
+    document.getElementById("answer-0").innerText = question["respuestas"][0];
+    document.getElementById("answer-1").innerText = question["respuestas"][1];
+    document.getElementById("answer-2").innerText = question["respuestas"][2];
+    document.getElementById("answer-3").innerText = question["respuestas"][3];
 }
 
-function preguntaRandom(categoriaActual, dificultad) {
-    let listaPreguntas = preguntas[categoriaActual][dificultad];
-    if (listaPreguntas.length === 0) {
-        document.getElementById("pregunta").innerText = "Haz alcanzado la Sabiduría (no hay más preguntas)";
-        finDelJuego();
+function randomQuestion(currentCategory, difficulty) {
+    let questionList = questions[currentCategory][difficulty];
+    if (questionList.length === 0) {
+        document.getElementById("question").innerText = "Haz alcanzado la Sabiduría (no hay más preguntas)";
+        gameOver();
     }
-    return listaPreguntas.splice(Math.floor(Math.random() * listaPreguntas.length), 1)[0];
+    return questionList.splice(Math.floor(Math.random() * questionList.length), 1)[0];
 }
 
-function categoriaRandom() {
-    let categoriasDisponibles = categorias.filter(cat => progreso[cat] < 3);
-    if (categoriasDisponibles.length === 0) return categorias[Math.floor(Math.random() * 4)];
-    return categoriasDisponibles[Math.floor(Math.random() * categoriasDisponibles.length)];
+function randomCategory() {
+    let availableCategories = categories.filter(cat => progress[cat] < 3);
+    if (availableCategories.length === 0) return categories[Math.floor(Math.random() * 4)];
+    return availableCategories[Math.floor(Math.random() * availableCategories.length)];
 }
 
-function iniciarTiempo() {
-    if (temporizador) clearInterval(temporizador);
-    tiempo = 21;
-    temporizador = setInterval(() => {
-        if (tiempo > 0) {
-            tiempo--;
-            document.getElementById("tiempo").innerText = `⏳ ${tiempo}s`;
+function startTimer() {
+    if (timer) clearInterval(timer);
+    time = 21;
+    timer = setInterval(() => {
+        if (time > 0) {
+            time--;
+            document.getElementById("time").innerText = `⏳ ${time}s`;
         } else {
-            document.getElementById("sonidoFin").play();
-            document.getElementById("pregunta").innerText = "TE QUEDASTE SIN TIEMPO!";
-            finDelJuego();
+            document.getElementById("game-over-sound").play();
+            document.getElementById("question").innerText = "TE QUEDASTE SIN TIEMPO!";
+            gameOver();
         }
     }, 1000);
 }
 
-function verificarRespuesta(respuestaSeleccionada) {
-    let respuesta = pregunta["correcta"];
-    document.getElementById("respuesta" + respuesta).classList.add("correcta");
-    if (respuestaSeleccionada === respuesta) {
-        document.getElementById("sonidoCorrecto").play();
-        racha++;
-        let multiplicador = racha >= 3 ? 2 : 1;
-        puntaje += 100 * multiplicador;
-        document.getElementById("respuesta" + respuesta).style.backgroundColor = "greenyellow";
-        document.getElementById("pregunta").innerText = "CORRECTO!";
-        if (categoriaActual === "Final Boss") {
-            progreso["FinalBoss"] = true;
+function checkAnswer(selectedAnswer) {
+    let answer = question["correcta"];
+    console.log(answer);
+    document.getElementById("answer-" + answer).classList.add("correct");
+    if (selectedAnswer === answer) {
+        document.getElementById("correct-sound").play();
+        streak++;
+        let multiplier = streak >= 3 ? 2 : 1;
+        score += 100 * multiplier;
+        document.getElementById("answer-" + answer).style.backgroundColor = "greenyellow";
+        document.getElementById("question").innerText = "CORRECTO!";
+        if (currentCategory === "Final Boss") {
+            progress["FinalBoss"] = true;
         } else {
-            progreso[categoriaActual]++;
+            progress[currentCategory]++;
         }
         
     } else {
-        document.getElementById("sonidoIncorrecto").play();
-        vidas--;
-        puntaje = Math.max(0, puntaje - 100);   
-        racha = 0;
-        document.getElementById("respuesta" + respuestaSeleccionada).style.backgroundColor = "red";
-        document.getElementById("respuesta" + respuesta).style.backgroundColor = "greenyellow";
-        document.getElementById("pregunta").innerText = "INCORRECTO!";
+        document.getElementById("incorrect-sound").play();
+        lives--;
+        score = Math.max(0, score - 100);   
+        streak = 0;
+        document.getElementById("answer-" + selectedAnswer).style.backgroundColor = "red";
+        document.getElementById("answer-" + answer).style.backgroundColor = "greenyellow";
+        document.getElementById("question").innerText = "INCORRECTO!";
     }
     setTimeout(() => {
-        document.getElementById("respuesta" + respuesta).classList.remove("correcta");
+        document.getElementById("answer-" + answer).classList.remove("correct");
     }, 1800);
-    actualizarProgreso();
-    document.getElementById("puntaje").innerText = `Puntaje: ${puntaje}`;
-    document.getElementById("vidas").innerText = "❤️".repeat(vidas) + "❌".repeat(3-vidas);
-    if (vidas <= 0) {
-        document.getElementById("sonidoFin").play();
-        document.getElementById("pregunta").innerText = "TE QUEDASTE SIN VIDAS!";
-        finDelJuego();
+    updateProgress();
+    document.getElementById("score").innerText = `Puntaje: ${score}`;
+    document.getElementById("lives").innerText = "❤️‍🔥".repeat(lives) + "❌".repeat(3-lives);
+    if (lives <= 0) {
+        document.getElementById("game-over-sound").play();
+        document.getElementById("question").innerText = "TE QUEDASTE SIN VIDAS!";
+        gameOver();
     } else {
         setTimeout(() => {
-            verificarProgreso();
+            checkProgress();
         }, 2000);
     }
 }
 
-function actualizarProgreso() {
-    let total = Object.values(progreso).reduce((a, b) => a + b, 0);
-    let porcentaje = (total / 12) * 100; 
-    document.getElementById("progreso").style.width = `${porcentaje}%`;
+function updateProgress() {
+    let total = Object.values(progress).reduce((a, b) => a + b, 0);
+    let porcentage = (total / 12) * 100; 
+    document.getElementById("progress").style.width = `${porcentage}%`;
 }
 
-function verificarProgreso() {
-    if (progreso["Historia de la Iglesia"] === 3 && progreso["Doctrina"] === 3 && progreso["Santos"] === 3 && progreso["Biblia"] === 3){
-        if (progreso["FinalBoss"] === true){
-            document.getElementById("categoria").innerText = "¡Ganaste!";
-            finDelJuego();
+function checkProgress() {
+    if (progress["Historia de la Iglesia"] === 3 && progress["Doctrina"] === 3 && progress["Santos"] === 3 && progress["Biblia"] === 3){
+        if (progress["FinalBoss"] === true){
+            document.getElementById("category").innerText = "¡Ganaste!";
+            gameOver();
         }
         else {
-            iniciarFinalBoss();
+            startFinalBoss();
         }
     }
     else {
-        iniciarJuego();
+        startGame();
     }
 }
 
-function iniciarFinalBoss() {
-    dificultad = "Final Boss";
-    categoriaActual = categoriaRandom();
-    pregunta = preguntaRandom(categoriaActual, dificultad);
+function startFinalBoss() {
+    difficulty = "Final Boss";
+    currentCategory = randomCategory();
+    question = randomQuestion(currentCategory, difficulty);
 
-    document.getElementById("categoria").innerText = `🔥 Final Boss: ${categoriaActual} 🔥`;
-    document.getElementById("pregunta").innerText = pregunta["pregunta"];
-    document.getElementById("respuesta0").innerText = pregunta["respuestas"][0];
-    document.getElementById("respuesta1").innerText = pregunta["respuestas"][1];
-    document.getElementById("respuesta2").innerText = pregunta["respuestas"][2];
-    document.getElementById("respuesta3").innerText = pregunta["respuestas"][3];
+    document.getElementById("category").innerText = `🔥 Final Boss: ${currentCategory} 🔥`;
+    document.getElementById("question").innerText = question["pregunta"];
+    document.getElementById("answer-0").innerText = question["respuestas"][0];
+    document.getElementById("answer-1").innerText = question["respuestas"][1];
+    document.getElementById("answer-2").innerText = question["respuestas"][2];
+    document.getElementById("answer-3").innerText = question["respuestas"][3];
 }
 
-function finDelJuego() {
-    clearInterval(temporizador);
-    tiempo = 20;
-    document.getElementById("pregunta").innerText = `¡Juego terminado! Tu puntaje final es: ${puntaje}`;
+function gameOver() {
+    clearInterval(timer);
+    time = 20;
+    document.getElementById("question").innerText = `¡Juego terminado! Tu puntaje final es: ${score}`;
     setTimeout(() => {
         window.location.href = "index.html";
     }, 3000);
 }
 
-function activarSonidoHover () {
-    document.getElementById("sonidoHoverBotón").currentTime = 0;
-    document.getElementById("sonidoHoverBotón").play();
+function buttonHoverSound () {
+    document.getElementById("button-hover-sound").currentTime = 0;
+    document.getElementById("button-hover-sound").play();
 }
 
-document.addEventListener("DOMContentLoaded", prepararJuego);
+document.addEventListener("DOMContentLoaded", prepareGame);
